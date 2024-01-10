94a99c63992529a48233f65da34fc9cf6496001e
minsolkim <123alsthf@naver.com>
2024-01-11 00:16:03 +0900

    searh bar

--- HomeEatPractice/Controller/TalkVC/InfoViewController.swift
+++ HomeEatPractice/Controller/TalkVC/InfoViewController.swift
@@ -8,23 +8,60 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "정보토크"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-        }()
+    //검색 뷰
+    lazy var SearchView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(named: "searchtf")
+        view.layer.cornerRadius = 7
+        view.layer.masksToBounds = true
+        
+        return view
+    } ()
+    // 텍스트 필드
+    lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "관심있는 집밥을 검색해보세요"
+        textField.font = UIFont.systemFont(ofSize: 13)
+        textField.textColor = UIColor(named: "searchfont")
+        textField.attributedPlaceholder = NSAttributedString(string: "관심있는 집밥을 검색해보세요", attributes: [NSAttributedString.Key.foregroundColor: UIColor(named: "searchfont") ?? UIColor.gray])
+
+        return textField
+    }()
+    // 검색 이미지
+    lazy var searchImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "search"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
 //정보토크
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
-        view.addSubview(titleLabel)
+        configUI()
+        
+        
+    }
+    func configUI() {
+        view.addSubview(SearchView)
+        SearchView.addSubview(searchTextField)
+        SearchView.addSubview(searchImageView)
+        SearchView.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        SearchView.widthAnchor.constraint(equalToConstant: 351).isActive = true
         NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            SearchView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            SearchView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20),
+            
+            searchTextField.leadingAnchor.constraint(equalTo: SearchView.leadingAnchor, constant: 10),
+            searchTextField.centerYAnchor.constraint(equalTo: SearchView.centerYAnchor),
+            searchTextField.trailingAnchor.constraint(equalTo: searchImageView.leadingAnchor, constant: -10),
+
+            searchImageView.trailingAnchor.constraint(equalTo: SearchView.trailingAnchor, constant: -10),
+            searchImageView.centerYAnchor.constraint(equalTo: SearchView.centerYAnchor),
+            searchImageView.widthAnchor.constraint(equalToConstant: 14.95), // Adjust the width as needed
+            searchImageView.heightAnchor.constraint(equalToConstant: 14.95) // Adjust the height as needed
             ])
     }
 
