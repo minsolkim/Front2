8a106e47fb88b9c220762631de9618160f02b4c9
minsolkim <123alsthf@naver.com>
2024-01-23 17:16:08 +0900

    commit Meal 코드 복붙

---
+++ HomeEatPractice/Controller/TalkVC/MealVC/Controller/MealWritingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MealWritingViewController.swift
+//  HomeEatPractice
+//
+//  Created by 이지우 on 2024/01/22.
+//
+
+import UIKit
+
+class MealWritingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
